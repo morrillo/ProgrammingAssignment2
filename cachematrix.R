dd6dfe113cfd9701c0822b9ddcdd8f26b98e7f23
morrillo <gustavo.orrillo@moldeo.coop>
2014-11-22 13:20:02 -0200

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+		}
+	get <- function() x
+	setinverse <- function(x) i <- x
+	getinverse <- function() i
+	list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+	
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+	library('MASS')
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()
+	if(!is.null(i)) {
+		print('getting cached data')
+		return(i)
+		}
+	data <- x$get()
+	i <- ginv(data)
+	x$setinverse(i)
+	i	
 }

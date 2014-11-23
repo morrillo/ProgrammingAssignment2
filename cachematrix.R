4808336ba3967cbe23d216480ffbd6f0849d38e1
morrillo <gustavo.orrillo@moldeo.coop>
2014-11-23 12:04:12 -0200

    Fixes

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,22 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# Builds list with matrix that holds setters and getters for the matrix data functions and setters and getters for the inverse information
 makeCacheMatrix <- function(x = matrix()) {
+	# sets inverse data to null
 	i <- NULL
+	# setter function
 	set <- function(y) {
 		x <<- y
 		i <<- NULL
 		}
+	# getter function for matrix data
 	get <- function() x
-	setinverse <- function(x) i <- x
+	# sets inverse information
+	setinverse <- function(x) {
+		i <<- x
+		}
+	# gets inverse function
 	getinverse <- function() i
 	list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 	
@@ -20,15 +27,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+	# loads package with ginv function
 	library('MASS')
-        ## Return a matrix that is the inverse of 'x'
+        ## Obtains inverse
 	i <- x$getinverse()
 	if(!is.null(i)) {
-		print('getting cached data')
-		return(i)
-		}
-	data <- x$get()
-	i <- ginv(data)
-	x$setinverse(i)
-	i	
+                print("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- ginv(data)
+        x$setinverse(i)
+	i 
 }
+
